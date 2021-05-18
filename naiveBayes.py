a4c87227ecc933f42e9a4001fc7f4e1782bc8f7d
TimotheusHuang <14151468+TimotheusHuang@users.noreply.github.com>
2021-05-18 15:16:15 -0400

    Add files via upload

---
+++ naiveBayes.py
@@ -0,0 +1,143 @@
+# naiveBayes.py
+# -------------
+# Licensing Information: Please do not distribute or publish solutions to this
+# project. You are free to use and extend these projects for educational
+# purposes. The Pacman AI projects were developed at UC Berkeley, primarily by
+# John DeNero (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# For more info, see http://inst.eecs.berkeley.edu/~cs188/sp09/pacman.html
+
+import util
+import classificationMethod
+import math
+
+class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
+  """
+  See the project description for the specifications of the Naive Bayes classifier.
+  
+  Note that the variable 'datum' in this code refers to a counter of features
+  (not to a raw samples.Datum).
+  """
+  def __init__(self, legalLabels):
+    self.legalLabels = legalLabels
+    self.type = "naivebayes"
+    self.k = 1 # this is the smoothing parameter, ** use it in your train method **
+    self.automaticTuning = False # Look at this flag to decide whether to choose k automatically ** use this in your train method **
+    
+  def setSmoothing(self, k):
+    """
+    This is used by the main method to change the smoothing parameter before training.
+    Do not modify this method.
+    """
+    self.k = k
+
+  def train(self, trainingData, trainingLabels, validationData, validationLabels):
+    """
+    Outside shell to call your method. Do not modify this method.
+    """  
+      
+    # might be useful in your code later...
+    # this is a list of all features in the training set.
+    self.features = list(set([ f for datum in trainingData for f in datum.keys() ]))
+    #self.features = trainingData[0].keys() 
+    if (self.automaticTuning):
+        kgrid = [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 50]
+    else:
+        kgrid = [self.k]
+        
+    self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, kgrid)
+      
+  def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, kgrid):
+    """
+    Trains the classifier by collecting counts over the training data, and
+    stores the Laplace smoothed estimates so that they can be used to classify.
+    Evaluate each value of k in kgrid to choose the smoothing parameter 
+    that gives the best accuracy on the held-out validationData.
+    
+    trainingData and validationData are lists of feature Counters.  The corresponding
+    label lists contain the correct label for each datum.
+    
+    To get the list of all possible features or labels, use self.features and 
+    self.legalLabels.
+    """
+
+    "*** YOUR CODE HERE ***"
+    msg = "kgrid = {0}"
+    print(msg.format(kgrid))
+    # print(trainingLabels)
+    kgrid = 0.01
+    self.prior = util.Counter()
+    self.prior.incrementAll(trainingLabels, 1)
+    
+    self.conditional = {}
+    for label in self.legalLabels:
+      self.conditional[label] = util.Counter() 
+    for i in range(0,len(trainingData)):
+      for (k,v) in  trainingData[i].items():
+        if v == 1:
+          self.conditional[trainingLabels[i]].incrementAll([k],1)
+    # smoothing
+    for label in self.legalLabels:
+      self.conditional[label].incrementAll(self.features,kgrid)
+      self.conditional[label].divideAll(self.prior[label]+2*kgrid)
+
+    # print(self.conditional)
+
+    # util.raiseNotDefined()
+        
+  def classify(self, testData):
+    """
+    Classify the data based on the posterior distribution over labels.
+    
+    You shouldn't modify this method.
+    """
+    guesses = []
+    self.posteriors = [] # Log posteriors are stored for later data analysis (autograder).
+    for datum in testData:
+      posterior = self.calculateLogJointProbabilities(datum)
+      guesses.append(posterior.argMax())
+      self.posteriors.append(posterior)
+    return guesses
+      
+  def calculateLogJointProbabilities(self, datum):
+    """
+    Returns the log-joint distribution over legal labels and the datum.
+    Each log-probability should be stored in the log-joint counter, e.g.    
+    logJoint[3] = <Estimate of log( P(Label = 3, datum) )>
+    
+    To get the list of all possible features or labels, use self.features and 
+    self.legalLabels.
+    """
+    logJoint = util.Counter()
+    for label in self.legalLabels:
+      logJoint[label] = 0
+      logJoint[label]+=math.log(self.prior[label])
+      #for i in self.conditional[label]:
+      for (k,v) in datum.items():
+        if v==0:
+          logJoint[label]+=math.log(1-self.conditional[label][k])
+        elif v==1:
+          logJoint[label]+=math.log(self.conditional[label][k])
+
+    #for label in self.legalLabels
+    "*** YOUR CODE HERE ***"
+    #util.raiseNotDefined()
+    
+    return logJoint
+  
+  def findHighOddsFeatures(self, label1, label2):
+    """
+    Returns the 100 best features for the odds ratio:
+            P(feature=1 | label1)/P(feature=1 | label2) 
+    
+    Note: you may find 'self.features' a useful way to loop through all possible features
+    """
+    featuresOdds = []
+       
+    "*** YOUR CODE HERE ***"
+    util.raiseNotDefined()
+
+    return featuresOdds
+    
+
+    
+      
